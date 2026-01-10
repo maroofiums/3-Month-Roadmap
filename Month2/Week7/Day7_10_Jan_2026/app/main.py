2a4b42b7756051d572022b6e598736b118f5f06d
maroof2424 <maroof96965@gmail.com>
2026-01-10 20:42:28 +0500

    Month2 Week7 day7

--- Month2/Week7/Day7_10_Jan_2026/app/main.py
+++ Month2/Week7/Day7_10_Jan_2026/app/main.py
@@ -2,16 +2,18 @@ from auth import fake_user, create_access_token
 from todo import router as todo_router
 from fastapi import FastAPI, HTTPException, Depends
 from middleware import logging_middleware
+from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI()
 app.middleware("http")(logging_middleware)
 
 @app.post("/login")
-def login(username: str, password: str):
-    if username == fake_user["username"] and password == fake_user["password"]:
-        access_token = create_access_token(data={"sub": username})
-        return {"access_token": access_token, "token_type": "bearer"}
-    else:
-        raise HTTPException(status_code=401, detail="Invalid username or password")
-    
-app.include_router(todo_router, prefix="/todos", tags=["todos"])
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    username = form_data.username
+    password = form_data.password
+
+    if username != fake_user["username"] or password != fake_user["password"]:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"sub": username})
+    return {"access_token": token, "token_type": "bearer"}
